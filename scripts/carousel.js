c1530100f0d8c4a4fa298bf7349ea7e9ae6ff72c
Raff Julius Martinez <raffjulius.martinez@outlook.com>
2021-05-07 19:51:53 +0800

    update

---
+++ scripts/carousel.js
@@ -0,0 +1,25 @@
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides() {
+  var slides = $(".mySlides");
+  var dots = $(".dot");
+  slideIndex += 1;
+  slideIndex = slideIndex > slides.length ? 1 : slideIndex;
+  slides.hide();
+  dots.removeClass("active");
+
+  $(".mySlides:eq(" + slideIndex + ")").show();
+  $(".dot:eq(" + slideIndex + ")").addClass("active");
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
