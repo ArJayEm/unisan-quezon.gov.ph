e1d6d68b0733388752cb4a9fb9ed32a7570a6176
Raff Julius Martinez <raffjulius.martinez@outlook.com>
2021-05-08 10:13:25 +0800

    carousel bg img

--- scripts/carousel_auto.js
+++ scripts/carousel_auto.js
@@ -1,29 +1,29 @@
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var slides = $(".mySlides");
   var dots = $(".dot");
   slides.css("display", "none");
-  slideIndex = slideIndex > slides.length ? 1 : slideIndex;
+  slideIndex = slideIndex > slides.length ? 1 : slideIndex + 1;
   dots.removeClass("active");
   $(".mySlides:eq(" + (slideIndex - 1) + ")").css("display", "block");
   $(".dot:eq(" + (slideIndex - 1) + ")").addClass("display", "block");
 
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  // var i;
+  // var slides = document.getElementsByClassName("mySlides");
+  // var dots = document.getElementsByClassName("dot");
+  // for (i = 0; i < slides.length; i++) {
+  //   slides[i].style.display = "none";
+  // }
+  // slideIndex++;
+  // if (slideIndex > slides.length) {
+  //   slideIndex = 1;
+  // }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  // slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
